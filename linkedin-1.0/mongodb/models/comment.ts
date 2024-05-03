50cfcc1a8ba32386391cd7bb548fa03464d6cb4b
nima memarzadeh <nmemarcoding@gmail.com>
2024-05-03 11:43:23 -0700

    Add comment model to MongoDB

--- linkedin-1.0/mongodb/models/comment.ts
+++ linkedin-1.0/mongodb/models/comment.ts
@@ -1,8 +1,29 @@
 import {IUser} from "@/types/user";
 import mongoose, { Schema, Document, models, Model } from "mongoose";
 
-export interface IPostBase {
+export interface ICommentBase {
     user: IUser;
     text: string;
     
 }
+
+export interface Icomment extends Document, ICommentBase {
+    createAt: Date;
+    updateAt: Date;
+}
+
+const commentSchema = new Schema<Icomment>({
+    user:{
+        userId: {type: String, required: true},
+        userImage: {type: String, required: true},
+        firstName: {type: String, required: true},
+        lastName: {type: String}
+    },
+    text: {type: String, required: true},
+    }, 
+    {
+        timestamps: true
+    
+});
+
+export const comment =mongoose.model<Icomment>("Comment", commentSchema);
